52faae07b47221ccb80f98dbfa35274eae293d57
Colin Perkins <csp@csperkins.org>
2018-10-18 15:20:29 +0100

    Update copyright

--- bin/latex-build.sh
+++ bin/latex-build.sh
@@ -2,10 +2,10 @@
 # =================================================================================================
 # Script to build a LaTeX document
 # 
-# Colin Perkins
+# Written by Colin Perkins
 # https://csperkins.org/
 #
-# Copyright (C) 2003-2017 University of Glasgow
+# Copyright (C) 2003-2018 University of Glasgow
 # 
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions 
