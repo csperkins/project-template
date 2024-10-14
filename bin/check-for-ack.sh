536cc1124afa11a52d062401ca9a6f35f38c3d5d
Colin Perkins <csp@csperkins.org>
2024-10-14 23:05:08 +0100

    Check rights retention statemnt

--- bin/check-for-ack.sh
+++ bin/check-for-ack.sh
@@ -14,5 +14,17 @@ do
     echo "WARNING: missing acknowledgements section?"
     tput sgr0    || true
   fi
+
+  # From https://www.gla.ac.uk/myglasgow/openresearch/openaccess/openaccessprocessatglasgow/
+  # Submissions MUST include:
+  # "For the purpose of open access, the author(s) has applied a Creative
+  # Commons Attribution (CC BY) licence to any Author Accepted Manuscript
+  # version arising from this submission."
+  c=`egrep -c -i -n 'CC BY' $i`
+  if [ $c != 1 ]; then
+    tput setaf 1 || true
+    echo "WARNING: missing rights retention statement?"
+    tput sgr0    || true
+  fi
 done
 
